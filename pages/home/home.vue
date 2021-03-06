8a84cd3553167d83d6f755e2fbfa90e745da408d
谢雨留明 <373289962@qq.com>
2021-03-06 22:29:39 +0800

    完成了tabbar的开发

---
+++ pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<view>首页组件</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/mine/mine.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<view>个人中心</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
